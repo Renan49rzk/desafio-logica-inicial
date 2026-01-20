5d2a2723b9167a1057b6c2e8ae0089c5a8551327
= <=>
2026-01-20 13:36:43 -0300

    desafio

---
+++ desafio.js
@@ -0,0 +1,29 @@
+let nomeHeroi = "Aragorn"
+let XP = 3500
+let nivel = ""
+
+if (XP <= 1000) {
+    nivel = "Ferro"
+} else if (XP >= 1001 && XP <= 2000) {
+    nivel = "Bronze"
+} else if (XP >= 2001 && XP <= 5000) {
+    nivel = "Prata"
+} else if (XP >= 5001 && XP <= 7000) {
+    nivel = "Ouro"
+} else if (XP >= 7001 && XP <= 8000) {
+    nivel = "Platina"
+} else if (XP >= 8001 && XP <= 9000) {
+    nivel = "Ascendente"
+} else if (XP >= 9001 && XP <= 10000) {
+    nivel = "Imortal"
+} else if (XP >= 10001) {
+    nivel = "Radiante"
+} else {
+    nivel = "Herói inválido"
+}
+
+console.log(
+  "O Herói de nome " + nomeHeroi + 
+  " possui " + XP + 
+  " de XP e está no nível " + nivel
+)
